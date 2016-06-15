349ff8966723dcf06a0ca3ba052d410c25def22d
kimbakcho <vngkgk624@naver.com>
2016-06-15 15:46:34 +0900

    ts make
    ts make

---
+++ Lang_en_US.ts
@@ -0,0 +1,328 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_US">
+<context>
+    <name>Configration_Form</name>
+    <message>
+        <location filename="configration_form.ui" line="14"/>
+        <source>환경설정</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="31"/>
+        <source>데이터베이스 설정</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="52"/>
+        <source>DB이름</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="71"/>
+        <source>포트</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="84"/>
+        <source>999.999.999.999</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="100"/>
+        <source>IP주소</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="113"/>
+        <source>사용자이름</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="126"/>
+        <source>암호</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="166"/>
+        <source>확인</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="179"/>
+        <source>적용</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="192"/>
+        <source>취소</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.ui" line="235"/>
+        <source>제목</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="119"/>
+        <source>Warning</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="119"/>
+        <source>Configration is empty.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="124"/>
+        <source>Apply</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="124"/>
+        <source>Configration Applied.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="179"/>
+        <source>Cancel</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="configration_form.cpp" line="179"/>
+        <source>Configration Canceled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="mainwindow.ui" line="14"/>
+        <source>QCClient</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="67"/>
+        <location filename="mainwindow.ui" line="74"/>
+        <location filename="mainwindow.ui" line="81"/>
+        <source>PushButton</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="100"/>
+        <source>화면</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="107"/>
+        <source>툴</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="111"/>
+        <source>언어</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="144"/>
+        <source>작업계획(Ctrl+&amp;W)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="147"/>
+        <source>Ctrl+W</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="156"/>
+        <source>온도현황판(Ctrl+&amp;T)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="159"/>
+        <source>Ctrl+T</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="174"/>
+        <source>한국어(Korean)(Ctrl+&amp;K)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="177"/>
+        <source>Ctrl+K</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="192"/>
+        <source>English(English)(Ctrl+&amp;E)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="195"/>
+        <source>Ctrl+E</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="204"/>
+        <source>환경설정(Ctrl+&amp;C)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="mainwindow.ui" line="207"/>
+        <source>Ctrl+C</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Temperature_Form</name>
+    <message>
+        <location filename="temperature_form.ui" line="14"/>
+        <source>온도현황판</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>WorkPlan_Form</name>
+    <message>
+        <location filename="workplan_form.ui" line="14"/>
+        <source>작업계획</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="47"/>
+        <source>기계이름</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="86"/>
+        <source>접속 상태</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="93"/>
+        <source>실행 상태</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="269"/>
+        <source>전자동</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="302"/>
+        <location filename="workplan_form.ui" line="523"/>
+        <source>남은시간</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="315"/>
+        <source>작업자</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="328"/>
+        <location filename="workplan_form.ui" line="536"/>
+        <source>공정시간</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="341"/>
+        <source>품목명</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="367"/>
+        <source>품목 코드 </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="380"/>
+        <source>품목코드</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="393"/>
+        <source>금형</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="408"/>
+        <source>작업자검색</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="427"/>
+        <source>작업자설정</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="446"/>
+        <source>캐비티수</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="465"/>
+        <source>금형설정</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="494"/>
+        <source>금형 이름</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="510"/>
+        <source>캐비티 수</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="815"/>
+        <source>목표량</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="837"/>
+        <source>양품</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="853"/>
+        <source>불량</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="1271"/>
+        <source>달성률</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="workplan_form.ui" line="1293"/>
+        <source>생산량</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>temp_moniter_style1</name>
+    <message>
+        <location filename="temp_moniter_style1.ui" line="14"/>
+        <source>Form</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="temp_moniter_style1.ui" line="36"/>
+        <source>245</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="temp_moniter_style1.ui" line="53"/>
+        <source>240</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="temp_moniter_style1.ui" line="88"/>
+        <source>250</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
